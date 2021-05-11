c0dbab06e5ba1c444f3c08e70975c2fc41423b18
saclod_github_007 <74409287+neil-github-007@users.noreply.github.com>
2021-05-11 18:44:53 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    fill("white");
+    text("C U T N I F O N", 700, 160);
+    fill("lightblue");
+    text("Hint: Performs a particular task", 700, 180);
+
+    fill("white");
+    text("A T A D A S B E", 100, 260);
+    fill("lightblue");
+    text("Hint: Stores information", 100, 280);
+
+    fill("white");
+    text("NOTE: Write your answers is uppercase", 250, 200);
+
+}
